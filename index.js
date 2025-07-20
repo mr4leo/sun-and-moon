b9908520079ab0979322683ef981db8d9ff99c91
mr4leo <152512581+mr4leo@users.noreply.github.com>
2025-07-20 03:17:56 -0400

    I added tap animation reset

---
+++ index.js
@@ -0,0 +1,6 @@
+let contentBtn = document.querySelector(".content")
+contentBtn.addEventListener("click", function(){
+contentBtn.classList.remove("content-animated")    
+void contentBtn.offsetWidth
+contentBtn.classList.add("content-animated")
+})
